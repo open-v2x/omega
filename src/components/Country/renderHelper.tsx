965f9124a92be769c241a39f994993b83b857382
liyoumin <li.youmin@99cloud.net>
2022-12-15 15:36:21 +0800

    fix: fix the install area format

--- src/components/Country/renderHelper.tsx
+++ src/components/Country/renderHelper.tsx
@@ -12,12 +12,20 @@ export const renderAreaFormItem = (
   }
   const status = form.getFieldValue('state');
   if (status !== 'open') {
-    return <Country {...rest} params={params || { cascade: true, needIntersection: false }} />;
+    return <Country {...rest} params={params || { cascade: true, needIntersection: true }} />;
   }
   return defaultRender(_);
 };
 
-export const renderAreaFormatName = (data: any) => {
-  const { countryName = '', provinceName = '', cityName = '', areaName = '' } = data || {};
-  return `${countryName}${provinceName}${cityName}${areaName}`;
+export const renderAreaFormatName = (data: any, hideIntersection?: boolean) => {
+  const {
+    countryName = '',
+    provinceName = '',
+    cityName = '',
+    areaName = '',
+    intersectionName = '',
+  } = data || {};
+  return `${countryName}${provinceName}${cityName}${areaName}${
+    hideIntersection ? '' : intersectionName
+  }`;
 };
